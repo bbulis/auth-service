3bea85665a8fd041cdf61ff9afdd2f31ef54b0da
Benjamin Bulis <benjamin.bulis@outlook.at>
2024-10-01 22:29:37 +0200

    register logic

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { RegisterUserDto } from './models/user/registerUser.model';
-import { Response } from 'express';
 
 @Controller()
 export class UserController {
